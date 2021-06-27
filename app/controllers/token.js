3413054deef68bc47f7a4a1c31efc9a53e54c7e4
Sathiyanarayanan-M <sathiyastar2000@gmail.com>
2021-06-27 13:56:21 +0530

    Response with err

--- app/controllers/token.js
+++ app/controllers/token.js
@@ -8,7 +8,8 @@ dotenv.config();
 exports.verifyToken = async (token,req,res,next) => {
     if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
     jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        console.log(decoded)
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.',err:err });
         req.userId = decoded.id;
         return res.send({auth:true,token:req.userId})
     });
